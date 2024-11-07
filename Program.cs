513c03a65b9e1732305aa2678836749d27b171e4
Sergi-0 <144801688+Sergi-0@users.noreply.github.com>
2024-11-07 23:06:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace LAB4_MO
+{
+    public class lab4_balash
+    {
+        private double[] c;
+        private double[] a;
+        private double b;
+        private string[] rasp_x; // расположение иксов
+
+        public lab4_balash(double[] c, double[] a, double b)
+        {
+            int n = c.Length;
+            string[] nabor_x = new string[] { "X1", "X2", "X3", "X4", "X5", "X6", "X7", "X8" };
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (c[j] < c[j + 1]) // Сравниваем для убывания
+                    {
+                        // Меняем местами c[j] и c[j + 1]
+                        double tempC = c[j];
+                        c[j] = c[j + 1];
+                        c[j + 1] = tempC;
+
+                        // Также меняем местами соответствующие элементы в массиве a
+                        double tempA = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = tempA;
+
+                        string tempX = nabor_x[j];
+                        nabor_x[j] = nabor_x[j + 1];
+                        nabor_x[j + 1] = tempX;
+                    }
+                }
+            }
+            this.c = c;
+            this.a = a;
+            this.b = b;
+            this.rasp_x = nabor_x;
+        }
+
+        public List<double[]> Solution_Balash()
+        {
+            List<double[]> continue_roots = new List<double[]> { }; // узлы у которых есть продолжение в следующем слое
+            List<double[]> solve_roots = new List<double[]> { }; // узлы решения
+
+            double[] pred_el = new double[8];
+
+
+            if (check_root(pred_el)) 
+            {
+                solve_roots.Add(pred_el);
+                return solve_roots;
+            }
+                
+            else continue_roots.Add(pred_el);
+
+
+            while (continue_roots.Any()) // пока у нас остаются узлы, которые могут разветвлятся 
+            {
+                int length_c_r = continue_roots.Count;
+                for (int i = 0; i<length_c_r; i++) 
+                {
+                    int k = -1; // позиция первой единички слева в векторе решения
+                    int flag = -1; // флаг для остановки поиска решения на этом уровне
+
+                    if (solve_roots.Any())
+                    {
+                        for (int j = 0; j < 8; j++) if (solve_roots[solve_roots.Count - 1][j] == 1) k = j;
+                    }
+
+                    
+
+                    for (int t = 7; t >= k+1; t--) 
+                    {
+
+                        pred_el = new double[8];
+                        for (int j = 0; j < 8; j++) pred_el[j] = continue_roots[i][j];
+
+                        if (pred_el[t] == 1) continue;
+
+                        pred_el[t] = 1;
+
+                        if (check_root(pred_el))
+                        {
+                            solve_roots.Add(pred_el);
+                            flag = 1;
+                            break;
+                        }
+
+                        else continue_roots.Add(pred_el);
+                    }
+
+                    if (flag == 1) { flag = -1; break; }
+                }
+                continue_roots.RemoveRange(0,length_c_r);
+
+                int count_clone = 0; // удаление дубликатов узлов у которых есть продолжение
+                for(int i = 0; i < continue_roots.Count; i++) 
+                {
+                    for(int j = 0; j < continue_roots.Count; j++) 
+                    {
+                        for(int z = 0; z < 8; z++) 
+                        {
+                            if(i == j) continue;
+                            if (continue_roots[i][z] == continue_roots[j][z]) count_clone++;
+                        }
+
+                        if (count_clone == 8) continue_roots.RemoveAt(i);
+
+                        count_clone = 0;
+                    }
+                }
+
+            }
+
+            List<double[]> itog_solve = new List<double[]> { };
+            for(int i = 0; i < solve_roots.Count; i++) 
+            {
+                double[] temp = new double[8];
+                for(int j = 0;j < 8; j++) 
+                {
+                    string x = $"X{j+1}";  
+                    int k = Array.IndexOf(rasp_x, x);
+                    temp[j] = solve_roots[i][k];   
+                }
+                itog_solve.Add(temp);
+            }
+            return itog_solve;
+        }
+
+        private bool check_root(double[] root)
+        {
+            double sum = 0;
+            for (int i = 0; i < c.Length; i++)
+            {
+                sum += a[i] * root[i];
+            }
+            return sum<=b;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] c = new double[] { 7, 7, 9, 8, 10, 2, 10, 3 };
+            double[] a = new double[] { 4, 7, -2, -6, 5, 2, 4, -3 };
+            double b = -10;
+            lab4_balash k = new lab4_balash(c, a, b);
+
+            List<double[]> list = k.Solution_Balash();
+
+            for(int i = 0;i < list.Count;i++)
+            {
+                double F = 0;
+                for (int j = 0; j < 8; j++) 
+                {
+                    Console.Write($"X{j+1}: {list[i][j]}   ");
+                    F+= c[j] * list[i][j];
+                    if(j ==7) Console.Write($"   F: {F}");    
+                } 
+                Console.WriteLine();      
+            }
+
+        
+        }
+    }
+}
